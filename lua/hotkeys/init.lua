fca6ec1ab2abe23e5a5028eef18d975733e30279
BosEriko <bos.eriko@gmail.com>
2022-09-07 02:35:38 +0800

    add a way to replace all text

--- lua/hotkeys/init.lua
+++ lua/hotkeys/init.lua
@@ -26,7 +26,7 @@ map("", "<C-s>", ":w<CR>", { silent = true })
 -- nnoremap <silent> <C-k> :!open -a firefox "https://boseriko-professional.notion.site/boseriko-professional/92045538d4be4360a6486d9d8d5178aa?v=b50d8f4a46a748a19cd7988e1d235a2e" --args --kiosk<CR>
 map("", "<C-b>", ':let @+ = expand("%")<CR>', { silent = true })
 -- nnoremap <C-f> :CtrlSF<space>
--- nnoremap <C-g> :%s/old/new/g
+map("", "<C-g>", ':%s/old/new/g')
 -- nnoremap <expr> <C-p> (len(system('git rev-parse')) ? ':Files' : ':GFiles --exclude-standard --others --cached')."\<cr>"
 
 -- Leader Shortcuts
