56ba3d6441f1df96843d3ea25377f003d2490d52
BosEriko <bos.eriko@gmail.com>
2022-09-07 02:37:33 +0800

    add a way to reverse texts

--- lua/hotkeys/init.lua
+++ lua/hotkeys/init.lua
@@ -36,7 +36,7 @@ map("", "<C-g>", ':%s/old/new/g')
 map("n", "<leader>l", ":set list!<CR>", { silent = true })
 map("n", "<leader>n", ":noh<CR>", { silent = true })
 map("n", "<leader>o", ":'<,'>sort<CR>", { silent = true })
--- nnoremap <silent> <leader>r :g/^/m0<CR>
+map("n", "<leader>r", ":g/^/m0<CR>", { silent = true })
 -- nnoremap <silent> <leader>s :Startify<CR>
 -- nnoremap <silent> <leader>t :ToggleWorkspace<CR>
 map("n", "<leader>w", ":set wrap!<CR>", { silent = true })
