39f36e4503d5f565a0bca08251db14d6ec32dbd4
BosEriko <bos.eriko@gmail.com>
2022-09-09 03:51:13 +0800

    use single quote instead of double

--- lua/hotkeys/init.lua
+++ lua/hotkeys/init.lua
@@ -6,38 +6,38 @@ local let = vim.g
 function map(mode, lhs, rhs, opts)
   local options = { noremap = true }
   if opts then
-    options = vim.tbl_extend("force", options, opts)
+    options = vim.tbl_extend('force', options, opts)
   end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- Change mapleader
-let.mapleader = " "
+let.mapleader = ' '
 
 -- Control Shortcuts
-map("", "<C-\\>", ":NvimTreeToggle<CR>", { silent = true })
-map("", "<C-t>", ":NvimTreeFindFile<CR>", { silent = true })
-map("", "<C-h>", ":bp!<CR>", { silent = true })
-map("", "<C-l>", ":bn!<CR>", { silent = true })
-map("", "<C-x>", ":bd<CR>", { silent = true })
-map("", "<C-q>", ":q<CR>", { silent = true })
-map("", "<C-s>", ":w<CR>", { silent = true })
-map("", "<C-k>", ':!cmd.exe /c start "https://boseriko-professional.notion.site/boseriko-professional/92045538d4be4360a6486d9d8d5178aa?v=b50d8f4a46a748a19cd7988e1d235a2e" /b<CR>', { silent = true })
-map("", "<C-b>", ':let @+ = expand("%")<CR>', { silent = true })
-map("", "<C-f>", ":Telescope live_grep<CR>", { silent = true })
-map("", "<C-g>", ':%s/old/new/g')
-map("", "<C-p>", ":Telescope find_files<CR>", { silent = true })
+map('', '<C-\\>', ':NvimTreeToggle<CR>', { silent = true })
+map('', '<C-t>', ':NvimTreeFindFile<CR>', { silent = true })
+map('', '<C-h>', ':bp!<CR>', { silent = true })
+map('', '<C-l>', ':bn!<CR>', { silent = true })
+map('', '<C-x>', ':bd<CR>', { silent = true })
+map('', '<C-q>', ':q<CR>', { silent = true })
+map('', '<C-s>', ':w<CR>', { silent = true })
+map('', '<C-k>', ':!cmd.exe /c start "https://boseriko-professional.notion.site/boseriko-professional/92045538d4be4360a6486d9d8d5178aa?v=b50d8f4a46a748a19cd7988e1d235a2e" /b<CR>', { silent = true })
+map('', '<C-b>', ':let @+ = expand("%")<CR>', { silent = true })
+map('', '<C-f>', ':Telescope live_grep<CR>', { silent = true })
+map('', '<C-g>', ':%s/old/new/g')
+map('', '<C-p>', ':Telescope find_files<CR>', { silent = true })
 
 -- Leader Shortcuts
-map("n", "<leader>d", ":DD<CR>", { silent = true })
-map("n", "<leader>e", ":PackerSync<CR>", { silent = true })
-map("n", "<leader>g", ":Gitsigns blame_line<CR>", { silent = true })
-map("n", "<leader>l", ":set list!<CR>", { silent = true })
-map("n", "<leader>n", ":noh<CR>", { silent = true })
-map("n", "<leader>o", ":'<,'>sort<CR>", { silent = true })
-map("n", "<leader>r", ":g/^/m0<CR>", { silent = true })
-map("n", "<leader>f", ":HopChar1<CR>", { silent = true })
-map("n", "<leader>w", ":set wrap!<CR>", { silent = true })
+map('n', '<leader>d', ':DD<CR>', { silent = true })
+map('n', '<leader>e', ':PackerSync<CR>', { silent = true })
+map('n', '<leader>g', ':Gitsigns blame_line<CR>', { silent = true })
+map('n', '<leader>l', ':set list!<CR>', { silent = true })
+map('n', '<leader>n', ':noh<CR>', { silent = true })
+map('n', '<leader>o', ':'<,'>sort<CR>', { silent = true })
+map('n', '<leader>r', ':g/^/m0<CR>', { silent = true })
+map('n', '<leader>f', ':HopChar1<CR>', { silent = true })
+map('n', '<leader>w', ':set wrap!<CR>', { silent = true })
 
 -- Comments Shortcuts
 require('Comment').setup({
