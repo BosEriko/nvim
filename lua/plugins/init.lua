c178081ee6aa00c2239fcc8c248c7fd99b5087aa
BosEriko <bos.eriko@gmail.com>
2022-09-09 00:02:34 +0800

    add border rounded to packer

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -37,7 +37,7 @@ end
 packer.init({
     display = {
         open_fn = function()
-            return require("packer.util").float()
+            return require("packer.util").float({ border = 'rounded' })
         end,
     },
 })
