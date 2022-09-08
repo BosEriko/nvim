a719105ecc8a023f28b0b24ab284be6d0c4abd99
BosEriko <bos.eriko@gmail.com>
2022-09-09 02:09:53 +0800

    remove unwanted spacing

--- lua/hotkeys/init.lua
+++ lua/hotkeys/init.lua
@@ -4,11 +4,11 @@
 -- Make it more like vimrc
 local let = vim.g
 function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- Change mapleader
