71cbb1bdc452387a5881e7c5beae531062b8a3a7
BosEriko <bos.eriko@gmail.com>
2022-09-19 13:59:23 +0800

    use highlight instead of hardcoded for treesittercontext

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -8,5 +8,23 @@
 -- Theme
 vim.cmd[[colorscheme dracula]]
 
-vim.api.nvim_set_hl(0, 'TreesitterContext', { fg = "#ffffff", bg = "#262626" })
-vim.api.nvim_set_hl(0, 'TreesitterContextLineNumber', { fg = "#ffffff", bg = "#262626" })
+-- Color Default
+local function get_color_from_hl(hl_name, fallback)
+  local id = vim.api.nvim_get_hl_id_by_name(hl_name)
+  if not id then
+    return fallback
+  end
+
+  local foreground = vim.fn.synIDattr(vim.fn.synIDtrans(id), "fg")
+  if not foreground or foreground == "" then
+    return fallback
+  end
+
+  return foreground
+end
+
+-- Treesitter Context
+local treesitter_bg_color = get_color_from_hl("Folder", "#6272A4")
+local treesitter_fg_color = get_color_from_hl("CursorLineNr", "#F8F8F2")
+vim.api.nvim_set_hl(0, 'TreesitterContext', { fg = treesitter_fg_color, bg = treesitter_bg_color })
+vim.api.nvim_set_hl(0, 'TreesitterContextLineNumber', { fg = treesitter_fg_color, bg = treesitter_bg_color })
