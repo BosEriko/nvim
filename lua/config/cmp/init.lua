4f56ce606700b582e2b452d94b29d015abb6de7d
BosEriko <bos.eriko@gmail.com>
2022-10-01 10:52:48 +0800

    use tsserver for now on cmp

--- lua/config/cmp/init.lua
+++ lua/config/cmp/init.lua
@@ -65,6 +65,6 @@ cmp.setup.cmdline(':', {
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+require('lspconfig')['tsserver'].setup {
   capabilities = capabilities
 }
