32c513b209bb4ae07228b52c0917f34e9620565c
BosEriko <bos.eriko@gmail.com>
2024-03-08 15:28:19 +0800

    update prompt_prefix to avoid ?

--- lua/config/nvim/settings/telescope.lua
+++ lua/config/nvim/settings/telescope.lua
@@ -9,7 +9,7 @@ require('telescope').setup({
     }
   },
   defaults = {
-    prompt_prefix = '  ',
+    prompt_prefix = ' 󰍉 ',
     selection_caret = '  ',
     path_display = { 'smart' },
     file_ignore_patterns = {
