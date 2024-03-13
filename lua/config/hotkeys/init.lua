42174521c16ee599c34b9d027092c6506cb4db5d
BosEriko <bos.eriko@gmail.com>
2024-03-13 20:15:56 +0800

    add comments on hotkeys

--- lua/config/hotkeys/init.lua
+++ lua/config/hotkeys/init.lua
@@ -18,47 +18,47 @@ let.mapleader = ' '
 local wk = require("which-key")
 
 -- Control Shortcuts
-map('', '<C-\\>', ':NvimTreeToggle<CR>', { silent = true })
-map('', '<C-t>', ':NvimTreeFindFile<CR>', { silent = true })
-map('', '<C-h>', ':bp!<CR>', { silent = true })
-map('', '<C-l>', ':bn!<CR>', { silent = true })
-map('', '<C-x>', ':bd<CR>', { silent = true })
-map('', '<C-q>', ':q<CR>', { silent = true })
-map('', '<C-s>', ':w<CR>', { silent = true })
-map('', '<C-k>', ':WhichKey<CR>', { silent = true })
-map('', '<C-b>', ':let @+ = expand("%")<CR>', { silent = true })
-map('', '<C-f>', ':Telescope live_grep<CR>', { silent = true })
-map('', '<C-p>', ':Telescope find_files<CR>', { silent = true })
-map('', '<C-z>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>', { silent = true })
+map('', '<C-\\>', ':NvimTreeToggle<CR>', { silent = true })                                        -- Toggle NvimTree
+map('', '<C-t>', ':NvimTreeFindFile<CR>', { silent = true })                                       -- Find current file in NvimTree
+map('', '<C-h>', ':bp!<CR>', { silent = true })                                                    -- Previous buffer
+map('', '<C-l>', ':bn!<CR>', { silent = true })                                                    -- Next buffer
+map('', '<C-x>', ':bd<CR>', { silent = true })                                                     -- Close buffer
+map('', '<C-q>', ':q<CR>', { silent = true })                                                      -- Quit
+map('', '<C-s>', ':w<CR>', { silent = true })                                                      -- Save
+map('', '<C-k>', ':WhichKey<CR>', { silent = true })                                               -- Show key mappings
+map('', '<C-b>', ':let @+ = expand("%")<CR>', { silent = true })                                   -- Copy current file path to clipboard
+map('', '<C-f>', ':Telescope live_grep<CR>', { silent = true })                                    -- Live grep
+map('', '<C-p>', ':Telescope find_files<CR>', { silent = true })                                   -- Find files
+map('', '<C-z>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>', { silent = true })                        -- Toggle terminal
 
 -- Leader Shortcuts
-map('n', '<leader>n', ':noh<CR>', { silent = true })
-map('n', '<leader>d', ':DD<CR>', { silent = true })
-map('n', '<leader>e', ':PackerSync<CR>', { silent = true })
-map('n', '<leader>g', ':Gitsigns blame_line<CR>', { silent = true })
-map('n', '<leader>l', ':set list!<CR>', { silent = true })
-map('n', '<leader>o', ":'<,'>!tac<CR>", { silent = true })
-map('n', '<leader>r', ':g/^/m0<CR>', { silent = true })
-map('n', '<leader>f', ':HopChar1<CR>', { silent = true })
-map('n', '<leader>w', ':set wrap!<CR>', { silent = true })
+map('n', '<leader>n', ':noh<CR>', { silent = true })                                               -- Remove highlighted text
+map('n', '<leader>d', ':DD<CR>', { silent = true })                                                -- Delete line
+map('n', '<leader>e', ':PackerSync<CR>', { silent = true })                                        -- Update Packer packages
+map('n', '<leader>g', ':Gitsigns blame_line<CR>', { silent = true })                               -- Show Git blame for current line
+map('n', '<leader>l', ':set list!<CR>', { silent = true })                                         -- Toggle display of hidden characters
+map('n', '<leader>o', ":'<,'>!tac<CR>", { silent = true })                                         -- Reverse selected lines
+map('n', '<leader>r', ':g/^/m0<CR>', { silent = true })                                            -- Reverse order of lines
+map('n', '<leader>f', ':HopChar1<CR>', { silent = true })                                          -- Jump to character (Easymotion)
+map('n', '<leader>w', ':set wrap!<CR>', { silent = true })                                         -- Toggle text wrap
 
 -- Visual Shortcuts
-map('v', '<C-g>', [["hy:%s/<C-r>h//gc<left><left><left>]], { silent = true })
+map('v', '<C-g>', [["hy:%s/<C-r>h//gc<left><left><left>]], { silent = true })                      -- Interactive Search and Replace
 
 -- Comments Shortcuts
 require('Comment').setup({
   toggler = {
-    line = '<C-c>cc',
-    block = '<C-c>bc',
+    line = '<C-c>cc',                                                                              -- Comment/uncomment line
+    block = '<C-c>bc',                                                                             -- Comment/uncomment block
   },
   opleader = {
-    line = '<C-c>c',
-    block = '<C-c>b',
+    line = '<C-c>c',                                                                               -- Add line comment
+    block = '<C-c>b',                                                                              -- Add block comment
   },
   extra = {
-    above = '<C-c>cO',
-    below = '<C-c>co',
-    eol = '<C-c>cA',
+    above = '<C-c>cO',                                                                             -- Add line comment above
+    below = '<C-c>co',                                                                             -- Add line comment below
+    eol = '<C-c>cA',                                                                               -- Add comment to end of line
   },
 })
 
